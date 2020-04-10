3cd3db8d8d0e7f99df2d58e740cf649992700fb0
NiVanc <nico@izebra.be>
2020-04-10 17:03:59 +0200

    Use slots and dynamic component in orde to conditionally render the slots

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button class="btn btn-primary" @click="activeComponent = 'appBlue'">Load Blue Template</button>
+        <button class="btn btn-success" @click="activeComponent = 'appGreen'">Load Green Template</button>
+        <button class="btn btn-danger" @click="activeComponent = 'appRed'">Load Red Template</button>
+        <hr />
+        <component :is="activeComponent">
+          <h3 slot="blue">Welcome to the blue world!</h3>
+          <h3 slot="green">Welcome to the green world!</h3>
+          <h3 slot="red">Welcome to the red world!</h3>
+        </component>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+export default {
+  data() {
+    return {
+      activeComponent: "appBlue"
+    };
+  },
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  }
+};
 </script>
 
-<style>
+<style scoped>
+h3 {
+  font-style: italic;
+}
 </style>
